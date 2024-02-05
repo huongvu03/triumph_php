44cd8761947f5905c9aeb0d8363475c3b0d6fb68
hoang765218 <mvhoang765218@gmail.com>
2024-02-05 19:57:02 +0700

    cart

---
+++ php/user/cart.php
@@ -0,0 +1,83 @@
+<?php
+    session_start();
+    $user=$_SESSION["user"];
+    $userId=$_SESSION["userId"];
+    $conn= mysqli_connect("localhost","root","","triumph_php");
+    if(!$conn){
+        die("error connect db");
+    }
+    $query = "SELECT product.proId, product.proImg ,product.proName, product.proPrice, cart.cartQuantity 
+    FROM cart
+    JOIN product ON cart.proId = product.proId
+    WHERE cart.userId = $userId";
+    
+    $result= mysqli_query($conn,$query);
+    $cartList=array();
+    while($row=mysqli_fetch_array($result)){
+        $cartList[]=$row;
+    }
+    mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+        <h1>Cart List</h1>
+        <a href="productList.php" class="btn btn-success">
+        Product
+    </a>
+        <?php
+            if(count($cartList)==0){
+                echo "<h3>No records</h3>";
+            }else{
+        ?>
+        <table class="table table-hover table-dark">
+            <thead>
+            <tr>  
+                <th>Id</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+           <tbody>
+            <?php
+               foreach($cartList as $pro){
+            ?>
+            <tr> 
+                
+                <td><?=$pro["proId"]?></td>
+                <td><img src="<?=$pro["proImg"]?>" alt="" width="100px" height="100px" ></td>
+                <td><?=$pro["proName"]?></td>
+                <td><?=$pro["proPrice"]?></td>
+                <td><?=$pro["cartQuantity"]?></td>
+                <td>
+                <a href="process-product-increase.php?id=<?=$pro["proId"]?>" class="btn btn-danger">increase</a>
+                <a href="process-product-decrease.php?id=<?=$pro["proId"]?>" class="btn btn-danger">decrease</a>
+                </td>
+            </tr>
+        <?php  
+         }
+        ?>
+        </tbody>
+  
+        </table>
+    <?php
+
+    }
+    ?>
+    
+    </div>
+    
+
+</body>
+</html>
---
+++ php/user/process-product-increase.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$userId=$_SESSION["userId"];
+if(isset($_GET["proId"])){
+    $proId = $_GET["proId"];
+}
+$conn = mysqli_connect("localhost","root","","triumph_php");
+if(!$conn){
+    die("error connect db");
+}
+// kiểm tra xem bảng Cart có productId ko
+$checkQuery = "SELECT * FROM cart WHERE proId = $proId";
+$checkResult = mysqli_query($conn, $checkQuery);
+
+//nếu có thì update quantity của nó +1
+if (mysqli_num_rows($checkResult) > 0) {
+    
+    $updateQuery = "UPDATE cart SET proQuantity = proQuantity + 1 WHERE proId = $proId";
+    $updateResult = mysqli_query($conn, $updateQuery);
+
+}
+mysqli_close($conn);
+header("Location: cart.php");
+?>
